c59f92ec1f0a007678e41a01db301e6ea1617e19
jaiwanth123 <114085858+jaiwanth123@users.noreply.github.com>
2024-07-30 19:56:29 +0530

    Add files via upload

---
+++ okto -bounty/my-app/public/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const GOOGLE_CLIENT_ID = ""
+
+root.render(
+<React.StrictMode>
+  <GoogleOAuthProvider clientId = {GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
+</React.StrictMode>
+);
+
